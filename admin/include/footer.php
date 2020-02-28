69bbea7c468596beacc67525c5cfc1c764553aa3
Rushabh Narekar <rush7665@gmail.com>
2020-02-28 07:24:33 +0530

    COMMIT

---
+++ admin/include/footer.php
@@ -0,0 +1,10 @@
+	<!-- Footer -->
+	<footer>
+			<div class="row">
+				<div class="col-lg-12">
+					<br/>
+				<center><p>Copyright &copy; | COMPLAINT MANAGEMENT SYSTEM <b><a href="http://www.ssipgujarat.in/sgh201920/" target="_blank">SGH2020</a></b></p></center>
+				</div>
+			</div>
+			<!-- /.row -->
+		</footer>
---
+++ admin/include/header.php
@@ -0,0 +1,32 @@
+<div class="navbar navbar-fixed-top" >
+		<div class="navbar-inner" style="background-color: #3d00be">
+			<div class="container">
+				<a class="btn btn-navbar" data-toggle="collapse" data-target=".navbar-inverse-collapse">
+					<i class="icon-reorder shaded"></i>
+				</a>
+
+			  	<a class="brand" style="color: white" href="#">
+			  		CMS | Admin
+			  	</a>
+
+				<div class="nav-collapse collapse navbar-inverse-collapse">
+					<ul class="nav pull-right">
+						<li><a href="#" style="color: white">
+							Admin
+						</a></li>
+						<li class="nav-user dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+								<i class="fas fa-user-alt" >  </i> <b class="caret"></b>
+								
+							</a>
+							<ul class="dropdown-menu">
+								<li><a href="change-password.php">Change Password</a></li>
+								<li class="divider"></li>
+								<li><a href="logout.php">Logout</a></li>
+							</ul>
+						</li>
+					</ul>
+				</div><!-- /.nav-collapse -->
+			</div>
+		</div><!-- /navbar-inner -->
+	</div><!-- /navbar -->
